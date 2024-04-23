9bd53f3e74d84210ea0666d3d7c8ea2d94bc668e
klmnvan <126891264+klmnvan@users.noreply.github.com>
2024-04-23 03:19:54 +0300

    add form check

---
+++ script.js
@@ -0,0 +1,21 @@
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', function() {
+    event.preventDefault();
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const nameInput = document.getElementById('name');
+    const apartmentlInput = document.getElementById('apartment');
+    const framelInput = document.getElementById('frame');
+    const homeInput = document.getElementById('home');
+    const streetInput = document.getElementById('street');
+    const cityInput = document.getElementById('city');
+
+    if (emailInput.value === '' || phoneInput.value === '' || nameInput.value === '' || apartmentlInput.value === ''
+    || framelInput.value === '' || homeInput.value === ''  || streetInput.value === '' || cityInput.value === '') {
+        alert('Пожалуйста, заполните все поля формы!');
+    } else {
+        window.location.href = 'index.html';
+        alert('Оплачено!');
+    }
+});
